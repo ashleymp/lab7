54575e8e120a0f8a1fa9b6bfac29e270bf605bc7
Ashley Perez <amp016@ucsd.edu>
2015-02-19 21:58:05 +0000

    Completed Lab7. It was fun

--- routes/project.js
+++ routes/project.js
@@ -4,7 +4,10 @@ exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
 
   // query for the specific project and
+  models.Project
+    .find({"_id":projectID})
   // call the following callback
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -17,12 +20,27 @@ exports.addProject = function(req, res) {
   console.log(form_data);
 
   // make a new Project and save it to the DB
+  var newProject = new models.Project(form_data);
+  newProject.save(afterSaving);
   // YOU MUST send an OK response w/ res.send();
+  function afterSaving(err) {
+    if(err) console.log(err);
+    res.send();
+  }
 }
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
   // find the project and remove it
+  models.Project
+    .find({"_id":projectID})
+    .remove()
+    .exec(afterRemoving);
+
+    function afterRemoving(err){
+      if(err) console.log(err);
+      res.send()
+    }
   // YOU MUST send an OK response w/ res.send();
 }
